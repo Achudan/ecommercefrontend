296ecfd1c78471a0190451a8c1b99622c503d7f8
Achudan <achuts4@gmail.com>
2020-09-27 23:47:18 +0530

    commit signin

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,69 +1,86 @@
 import React from 'react';
-import { Redirect} from 'react-router-dom'
-// import axios from 'axios';
+import { Redirect } from 'react-router-dom'
+import axios from 'axios';
 
-class SignInPage extends React.Component{
-    constructor(){
+class SignInPage extends React.Component {
+    constructor() {
         super();
-        this.state={
+        this.state = {
             emailid: "",
             password: "",
             loginAccepted: false,
             userId: "",
+            errorMessage:"",
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange = (event)=>{
+    handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        this.setState({[name]:value})
+        this.setState({ [name]: value })
     }
 
-    handleSubmit = (event) =>{
+    handleSubmit = (event) => {
         event.preventDefault();
-        console.log("Successfully submitted");
-        //axios call for sign in validation
-        //if successful navigate to home page and send id in props
-        const userIdFromAPI = "1234";
-        this.setState({loginAccepted:true, userId:userIdFromAPI})
-        //else wrong password error
-    }
 
-    render() {
-        let afterLogin;
-        
-        if(this.state.loginAccepted){
-            console.log(this.state.userId)
-            afterLogin = (
-                // <HomePage userId={this.state.userId} />
-                // <BrowserRouter>
-                    <Redirect to={{pathname:`/${this.state.userId}` , state:{userid:this.state.userId}}}/>
-                // </BrowserRouter>
-            )
+        const signInDetails = {
+            "emailid": this.state.emailid,
+            "password": this.state.password
         }
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Email Id:
-                    <input type="text" name="emailid" value={this.state.emailid} onChange={this.handleChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Password:
-                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-                    </label>
-                    <br />
-                    <input type="submit" value="Sign In" />
-                </form>
-                {
-                    afterLogin
-                }
-            </div>
+        if(this.state.emailid === "" || this.state.password===""){
+            this.setState({errorMessage:"Fields cannot be null.", loginAccepted: false})
+            return
+        }
+        
+        axios.post(
+            "http://localhost:5000/signin",
+            signInDetails,
+            
+        ).then(
+            res => {
+                console.log(res.data)
+                this.setState({userId:res.data, loginAccepted: true})
+            }
+        ).catch((error)=>{
+            console.log(error.response)
+            if(error.reponse == null){
+                this.setState({errorMessage:error.response.data, loginAccepted: false})
+            }
+        }
         )
+}
+
+render() {
+    let afterLogin;
+    if (this.state.loginAccepted) {
+        afterLogin = (<Redirect to={{ pathname: `/${this.state.userId}`, state: { userid: this.state.userId } }} />)
     }
+    else{
+    afterLogin = (<p>{this.state.errorMessage}</p>)
+    }
+    return (
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Email Id:
+                    <input type="text" name="emailid" value={this.state.emailid} onChange={this.handleChange} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                </label>
+                <br />
+                <input type="submit" value="Sign In" />
+            </form>
+            {
+                afterLogin
+            }
+        </div>
+    )
+}
 }
 
 export default SignInPage
